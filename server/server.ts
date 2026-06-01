f218fb8c82da3e2ad0fdfcbd1b50f33c4dac263e
Robb-Designs <rtlee@aggies.ncat.edu>
2026-06-01 14:01:00 -0400

    feat(server): start Express only after successful MongoDB connection with async

--- server/server.ts
+++ server/server.ts
@@ -3,11 +3,22 @@
 // Dependencies
 import app from './src/app';
 import dotenv from 'dotenv';
+import connectDB from './src/config/db';
 
 dotenv.config();
 const PORT = Number(process.env.PORT) || 5000; // Number: explicity for TypeScript
 
-// PORT
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-})
+//Database Connection
+const startServer = async () => {
+     try {
+      await connectDB();
+      app.listen(PORT, () => {
+         console.log(`Server running on http://localhost:${PORT}`)
+      });
+   } catch(error) {
+      console.error('Connection Error: ', error);
+   }
+};
+
+startServer();
+
