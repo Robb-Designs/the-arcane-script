7790e9fd2e2477195f11b99277105fb8bb98d437
Robb-Designs <rtlee@aggies.ncat.edu>
2026-06-01 15:27:03 -0400

    refactor(user-model): strongly type User schema with IUser interface and gameplay stat fields

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -5,9 +5,14 @@ interface IUser {
     username: string,
     email: string,
     password: string,
+    wins: number;
+    losses: number;
+    totalMatches: number;
+    averageAccuracy: number;
+    highestWpm: number;
 }
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema<IUser>({
     username: { type: String, trim: true, required: true, unique: true },
     email: { type: String, trim: true, lowercase: true, required: true, unique: true },
     password: { type: String, minlength: 8, required: true },
