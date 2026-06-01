ea5ea3186aab0908202d60e6343c323d1a022b76
Robb-Designs <rtlee@aggies.ncat.edu>
2026-06-01 13:43:26 -0400

    chore(db): add DNS resolver for testing and harden MongoDB connection

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -1,1 +1,27 @@
 // Responsible for Mongo connection
+
+//setting dns for testing
+const dns = require('dns');
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+
+
+import mongoose from 'mongoose';
+import { error } from 'node:console';
+
+
+async function connectDB(){
+    try {
+        if(!process.env.MONGO_URI){
+            throw new Error("DATABASE URI is missing");
+        }
+
+        const response = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB Connection Successful`);
+
+    } catch (error) {
+        console.error('MongoDB Connection Failed: ', error);
+        process.exit(1); // terminates app from running. 1: error
+    }
+}
+
+export default connectDB;
