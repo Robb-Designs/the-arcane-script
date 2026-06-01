8d0d9a077157f945e161c12145f316f207056280
Robb-Designs <rtlee@aggies.ncat.edu>
2026-06-01 16:47:38 -0400

    feat(auth): add auth router with register/login placeholder endpoints and

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,11 +3,13 @@
 // Dependencies
 import express from 'express';
 import cors from 'cors';
+import authRoutes from './routes/authRoutes';
 const app = express();
 
 // MidddleWare
 app.use(cors()); // Allows my React frontend to talk to my Express backend
 app.use(express.json()); // Parsing the incoming json before reaching any routes
+app.use('/api/auth', authRoutes)
 
 
 // Routes
