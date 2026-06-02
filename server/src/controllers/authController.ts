3fdc908490f9658b304f5c26837322bdd060bfa6
Robb-Designs <rtlee@aggies.ncat.edu>
2026-06-02 02:02:30 -0400

    feat(auth): implement login flow with email lookup and bcrypt password verification, error handling

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -6,10 +6,9 @@ import { Request, Response } from 'express';
 import bcrypt from 'bcrypt';
 
 
-// Handle user signup request
+// reates a new account after validating required fields and checking for existing email or username
 async function registerUser(req: Request, res: Response) {
     try {
-        // Define required fields from request body
         const { username, email, password } = req.body;
 
         if (!username || !email || !password) {
@@ -50,8 +49,40 @@ async function registerUser(req: Request, res: Response) {
 
 async function loginUser(req: Request, res: Response) {
     try {
+        const { email, password } = req.body;
 
-    } catch {
+        if (!email || !password) {
+            res.status(400).json({
+                message: "Request Error: Account details missing"
+            });
+            return;
+        }
+
+        const user = await User.findOne({ email })
+        if (!user) {
+            res.status(401).json({
+                message: "Invalid credentials"
+            });
+            return;
+        }
+        // Compare incoming password against stored hash
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            res.status(401).json({
+                message: "Invalid credentials"
+            });
+            return;
+        }
+
+        return res.status(200).json({ message: "Login Successful!" });
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            message: "Internal Server Error"
+        });
 
     }
+
 }
