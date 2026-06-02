3dddadc0ea9a7e99e118ee37575671878bfc808a
Robb-Designs <rtlee@aggies.ncat.edu>
2026-06-02 10:03:08 -0400

    define interface for token payload structure

--- server/src/middleware/authMiddleware.ts
+++ server/src/middleware/authMiddleware.ts
@@ -1,1 +1,12 @@
 // Responsible for verifying JWT token
+// Dependencies
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+// Define interface structure for token payload
+interface AuthRequest extends Request {
+    user?: {
+        userId: string;
+    };
+}
+
