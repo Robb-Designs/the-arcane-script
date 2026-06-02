fb2d4c074f5ffb8196c08ac054320ef8b0825aea
Robb-Designs <rtlee@aggies.ncat.edu>
2026-06-02 16:27:46 -0400

    chore(server): wire auth and result routers into Express app configuration

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,12 +4,14 @@
 import express from 'express';
 import cors from 'cors';
 import authRoutes from './routes/authRoutes';
+import resultRoutes from './routes/resultRoutes';
 const app = express();
 
 // MidddleWare
 app.use(cors()); // Allows my React frontend to talk to my Express backend
 app.use(express.json()); // Parsing the incoming json before reaching any routes
 app.use('/api/auth', authRoutes)
+app.use('/api/results', resultRoutes);
 
 
 // Routes
