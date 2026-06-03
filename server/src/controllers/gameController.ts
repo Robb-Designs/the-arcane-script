866ac79cd589c5e051b4ed9c595283ed5426176d
Robb-Designs <rtlee@aggies.ncat.edu>
2026-06-03 11:12:25 -0400

    feat(server): scaffold battle start logic in game controller

--- server/src/controllers/gameController.ts
+++ server/src/controllers/gameController.ts
@@ -1,2 +1,15 @@
 // Responsible for handling battle init, generate enemy, generate typing prompts
 // Respond to game requests
+//Dependencies
+import Battle from "../models/Battle";
+import { Response } from "express";
+import AuthRequest from "../types/AuthRequest";
+import User from "../models/User";
+
+async function startBattle(req: AuthRequest, res: Response) {
+    try {
+        const playerId = req.user?.userId;
+    } catch (error) {
+
+    }
+}
