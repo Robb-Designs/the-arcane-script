74cb8d4e59f221a9ec0cb01ad42c5a23579d4ef0
Robb-Designs <rtlee@aggies.ncat.edu>
2026-06-03 17:35:37 -0400

    feat(client): add basic Navbar component with auth links

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+
+function Navbar() {
+  return (
+    <nav>
+      <h1>The Arcane Script</h1>
+
+      <ul>
+        {/* Auth links will go here */}
+        <li>Login</li>
+        <li>Register</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
