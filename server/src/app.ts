c20667f74a1a656a1eb270ea7b02b3cbeb1b215d
Robb-Designs <rtlee@aggies.ncat.edu>
2026-06-03 00:39:40 -0400

    feat(server): mount enemy routes in app and expose enemy API endpoint

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,13 +5,15 @@ import express from 'express';
 import cors from 'cors';
 import authRoutes from './routes/authRoutes';
 import resultRoutes from './routes/resultRoutes';
+import enemyRoutes from './routes/enemyRoutes';
 const app = express();
 
 // MidddleWare
 app.use(cors()); // Allows my React frontend to talk to my Express backend
 app.use(express.json()); // Parsing the incoming json before reaching any routes
-app.use('/api/auth', authRoutes)
+app.use('/api/auth', authRoutes);
 app.use('/api/results', resultRoutes);
+app.use('/api/enemies', enemyRoutes);
 
 
 // Routes
