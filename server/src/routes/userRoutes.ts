914897b985562b3a7f5e6ea34046cc9210697b62
Robb-Designs <rtlee@aggies.ncat.edu>
2026-06-03 16:43:30 -0400

    feat(server): add authenticated user profile route

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -1,1 +1,13 @@
 // User-related URLs.
+
+// Dependencies
+import { Router } from 'express';
+import tokenAuth from '../middleware/authMiddleware';
+import getProfile from '../controllers/userController';
+
+const userRoutes = Router();
+
+// Route Points
+userRoutes.get('/profile', tokenAuth, getProfile);
+
+export default userRoutes;
