6435f7e38a714362600ba952a692a6c69f79abc0
Robb-Designs <rtlee@aggies.ncat.edu>
2026-06-04 03:54:43 -0400

    feat(client): create initial Login page component structure

---
+++ client/src/pages/Login.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import { Button } from "@/components/ui/8bit/button";
+import { Input } from "@/components/ui/input";
+import heroImage from "@/assets/images/hero-background.webp";
+
+
+
+function Login() {
+  return (
+    <div>
+      Login
+    </div>
+  );
+}
+
+export default Login;
