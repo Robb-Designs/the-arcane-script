2a42e45cae69b114aeeb56b1fe1a1a1146eda8e2
Robb-Designs <rtlee@aggies.ncat.edu>
2026-06-03 23:25:36 -0400

    feat(client): add Hero section component

---
+++ client/src/components/Hero.tsx
@@ -0,0 +1,19 @@
+
+function Hero() {
+  return (
+    <section>
+      <h1>The Arcane Script</h1>
+
+      <p>
+        Battle mystical enemies through the power of typing.
+      </p>
+
+      <div>
+        <button>Begin Journey</button>
+        <button>Login</button>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
